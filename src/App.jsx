862eeac1cb44c8a7e387a8fc976918dfe620e941
Shivamahajan045 <111225580+Shivamahajan045@users.noreply.github.com>
2026-04-09 13:44:41 +0530

    load more data using dummy api

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import Accordian from "./components/accordian";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 function App() {
   return (
     <div className="app">
       {/* <Accordian />
       <RandomColor />
       <StarRating noofStars={10} /> */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+
+      <LoadMoreData />
     </div>
   );
 }
